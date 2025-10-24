3dd63e0c73a64185586df79923d0f8102c50ebf5
kseniy-san <ksyusankoa@yandex.ru>
2025-10-24 12:37:06 +0300

    new

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><a href="lab1/1_1.php">lab1</a></li>
+        <li><a href="lab2/">lab2</a></li>
+    </ul>
+    
+</body>
+</html>
--- phpinfo.php
+++ phpinfo.php
@@ -1,5 +1,2 @@
 <?php
-
-// BEGIN (write your solution here)
-print_r("- Did Joffrey agree?\n- He did. He also said \"I'm using this \\n\".");
-// END
+phpinfo();
