784beb474a0aaa3dcc6e0012266ad05461f4f1bf
kseniy-san <ksyusankoa@yandex.ru>
2025-10-24 12:40:36 +0300

    lab

--- index.php
+++ index.php
@@ -3,13 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Лабораторные работы</title>
 </head>
 <body>
     <ul>
         <li><a href="lab1/1_1.php">lab1</a></li>
-        <li><a href="lab2/">lab2</a></li>
+        <li><a href="lab2/1_2.php">lab2</a></li>
+        <li><a href="lab1/1_3.php">lab1</a></li>
+        <li><a href="lab1/1_4.php">lab1</a></li>
+        <li><a href="lab1/1_5.php">lab1</a></li>
     </ul>
-    
+    <ul>
+        <li><a href="lab2/2_1.php">lab1</a></li>
+        <li><a href="lab2/2_2.php">lab2</a></li>
+        <li><a href="lab2/2_3.php">lab1</a></li>
+        <li><a href="lab2/2_4.php">lab1</a></li>
+        <li><a href="lab2/2_5.php">lab1</a></li>
+        <li><a href="lab2/2_6.php">lab1</a></li>
+        <li><a href="lab2/2_7.php">lab1</a></li>
+        <li><a href="lab2/2_8.php">lab1</a></li>
+    </ul>
 </body>
 </html>
