69d52d36664733d95f85cc3e37072064de092e3d
kseniy-san <ksyusankoa@yandex.ru>
2025-11-18 16:18:56 +0300

    end32

--- lab3/3_2.php
+++ lab3/3_2.php
@@ -8,10 +8,109 @@ declare(strict_types=1);
 	- Создайте переменную $hour
 	- С помощью функции getdate() присвойте переменной $hour текущий час
 	*/
-$now = time();
-$birthday = mktime(13, 55, 00, 2, 21, 2006);
-$date_i = getdate();
-$hour = $date_i['hours'];
+
+
+function DateTime(): array
+{
+    $now = time();
+
+    $currentYear = (int)date('Y');
+    $birthday = mktime(13, 55, 00, 2, 21, $currentYear);
+
+    if ($birthday < $now) {
+        $birthday = mktime(13, 55, 00, 2, 21, date('Y') + 1);
+    }
+
+    $Date = getdate();
+    $hour = $Date['hours'];
+
+    return [
+        'now' => $now,
+        'birthday' => $birthday,
+        'hour' => $hour
+    ];
+}
+
+function getMessage(int $hour): string
+{
+    if ($hour >= 0 && $hour < 6) {
+        return 'Доброй ночи';
+    } elseif ($hour >= 6 && $hour < 12) {
+        return 'Доброе утро';
+    } elseif ($hour >= 12 && $hour < 18) {
+        return 'Добрый день';
+    } elseif ($hour >= 18 && $hour <= 23) {
+        return 'Добрый вечер';
+    } else {
+        return 'Доброй ночи';
+    }
+}
+
+function formatRussianDate(int $timestamp): string
+{
+    $months = [
+        1 => 'января',
+        2 => 'февраля',
+        3 => 'марта',
+        4 => 'апреля',
+        5 => 'мая',
+        6 => 'июня',
+        7 => 'июля',
+        8 => 'августа',
+        9 => 'сентября',
+        10 => 'октября',
+        11 => 'ноября',
+        12 => 'декабря'
+    ];
+
+    $daysOfWeek = [
+        'воскресенье',
+        'понедельник',
+        'вторник',
+        'среда',
+        'четверг',
+        'пятница',
+        'суббота'
+    ];
+
+    $date = getdate($timestamp);
+    $day = $date['mday'];
+    $month = $months[$date['mon']];
+    $year = $date['year'];
+    $dayOfWeek = $daysOfWeek[$date['wday']];
+    $time = date('H:i:s', $timestamp);
+
+    return "Сегодня $day $month $year года, $dayOfWeek $time";
+}
+
+function getBirthday(int $now, int $birthday): array
+{
+    $secondsLeft = $birthday - $now;
+
+    $days = floor($secondsLeft / (60 * 60 * 24));
+    $hours = floor(($secondsLeft % (60 * 60 * 24)) / (60 * 60));
+    $minutes = floor(($secondsLeft % (60 * 60)) / 60);
+    $seconds = $secondsLeft % 60;
+
+    return [
+        'days' => $days,
+        'hours' => $hours,
+        'minutes' => $minutes,
+        'seconds' => $seconds
+    ];
+}
+
+$data = DateTime();
+$now = $data['now'];
+$birthday = $data['birthday'];
+$hour = $data['hour'];
+
+$welcome = getMessage($hour);
+
+// Форматирование даты
+$formattedDate = formatRussianDate($now);
+
+$timeUntilBirthday = getBirthday($now, $birthday);
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -25,6 +124,7 @@ $hour = $date_i['hours'];
 
 <body>
     <h1>Использование функций даты и времени</h1>
+
     <?php
     /*
 	ЗАДАНИЕ 2
@@ -45,85 +145,22 @@ $hour = $date_i['hours'];
 	- На отдельной строке выведите фразу "До моего дня рождения осталось "
 	- Выведите количество дней, часов, минут и секунд оставшееся до Вашего дня рождения
 	*/
-
-    if ($hour >= 0 && $hour < 6) {
-        $welcome = 'Доброй ночи';
-    } elseif ($hour >= 6 && $hour < 12) {
-        $welcome = 'Доброе утро';
-    } elseif ($hour >= 12 && $hour < 18) {
-        $welcome = 'Добрый день';
-    } elseif ($hour >= 18 && $hour <= 23) {
-        $welcome = 'Добрый вечер';
-    } else {
-        $welcome = 'Доброй ночи';
-    }
-
-    echo "<p>$welcome</p>";
-
-    setlocale(LC_ALL, 'ru_RU.UTF-8');
-
-    /**
-     * Форматирует дату на русском языке
-     * 
-     * @param int $timestamp метка времени
-     * @return string отформатированная дата
-     */
-    function formatRussianDate(int $timestamp): string
-    {
-        $months = [
-            1 => 'января',
-            2 => 'февраля',
-            3 => 'марта',
-            4 => 'апреля',
-            5 => 'мая',
-            6 => 'июня',
-            7 => 'июля',
-            8 => 'августа',
-            9 => 'сентября',
-            10 => 'октября',
-            11 => 'ноября',
-            12 => 'декабря'
-        ];
-
-        $daysOfWeek = [
-            'воскресенье',
-            'понедельник',
-            'вторник',
-            'среда',
-            'четверг',
-            'пятница',
-            'суббота'
-        ];
-
-        $date = getdate($timestamp);
-        $day = $date['mday'];
-        $month = $months[$date['mon']];
-        $year = $date['year'];
-        $dayOfWeek = $daysOfWeek[$date['wday']];
-        $time = date('H:i:s', $timestamp);
-
-        return "Сегодня $day $month $year года, $dayOfWeek $time";
-    }
-    $formattedDate = formatRussianDate($now);
-    echo $formattedDate . "<br>";
-
-    $be = date('Y');
-    $bm = (int) $_POST['Birthday_Month'];
-    $bd = (int) $_POST['Birthday_day'];
-
-    $date = "$be-$bm-$bd";
-    $check_time = strtotime($date) - time();
-
-    $months = floor($check_time / 2592000);
-    $days = floor($check_time / 86400);
-    $hours = floor(($check_time % 86400) / 3600);
-    $minutes = floor(($check_time % 3600) / 60);
-
-    echo "Осталось минут: $minutes <br>\n";
-    echo "Осталось часов $hours<br>\n";
-    echo "Осталось дней $days<br>\n";
-    echo "Осталось месяцев $months<br>\n";
     ?>
+<h1><?= $welcome ?></h1>
+
+<p>
+    <b>Текущая дата и время:</b><br>
+    <?= $formattedDate ?>
+</p>
+
+<p>
+    <b>До моего дня рождения осталось:</b><br>
+    <?= $timeUntilBirthday['days'] ?> дней,
+    <?= $timeUntilBirthday['hours'] ?> часов,
+    <?= $timeUntilBirthday['minutes'] ?> минут,
+    <?= $timeUntilBirthday['seconds'] ?> секунд
+</p>
 </body>
 
 </html>
+
