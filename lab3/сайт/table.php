edc517e82731bdf1756d4ea373e4a4937344575d
kseniy-san <ksyusankoa@yandex.ru>
2025-11-18 17:49:04 +0300

    5

--- lab3/сайт/table.php
+++ lab3/сайт/table.php
@@ -1,5 +1,9 @@
+<?php
+require_once 'inc/lib.inc.php';
+?>
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +11,7 @@
   <title>Таблица умножения</title>
   <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
   <header>
     <!-- Верхняя часть страницы -->
@@ -22,63 +27,33 @@
     <form action=''>
       <label>Количество колонок: </label>
       <br>
-      <input name='cols' type='text' value=''>
+      <input name='cols' type='text' value='<?php echo isset($_GET['cols']) ? $_GET['cols'] : ''; ?>'>
       <br>
       <label>Количество строк: </label>
       <br>
-      <input name='rows' type='text' value=''>
+      <input name='rows' type='text' value='<?php echo isset($_GET['rows']) ? $_GET['rows'] : ''; ?>'>
       <br>
       <label>Цвет: </label>
       <br>
-      <input name='color' type='color' value='#ff0000' list="listColors">
-	<datalist id="listColors">
-		<option>#ff0000</option>/>
-		<option>#00ff00</option>
-		<option>#0000ff</option>
-	</datalist>
+      <input name='color' type='color' value='<?php echo isset($_GET['color']) ? $_GET['color'] : '#ff0000'; ?>' list="listColors">
+      <datalist id="listColors">
+        <option>#ff0000</option>
+        <option>#00ff00</option>
+        <option>#0000ff</option>
+      </datalist>
       <br>
       <br>
       <input type='submit' value='Создать'>
     </form>
     <br>
     <!-- Таблица -->
-    <table>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>4</td>
-        <td>6</td>
-        <td>8</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>6</td>
-        <td>9</td>
-        <td>12</td>
-        <td>15</td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>8</td>
-        <td>12</td>
-        <td>16</td>
-        <td>10</td>
-      </tr>
-      <tr>
-        <td>5</td>
-        <td>10</td>
-        <td>15</td>
-        <td>20</td>
-        <td>25</td>
-      </tr>
-    </table>
+   <?php 
+    $cols = isset($_GET['cols']) ? (int)$_GET['cols'] : 6;
+    $rows = isset($_GET['rows']) ? (int)$_GET['rows'] : 6;
+    $color = isset($_GET['color']) ? $_GET['color'] : 'lightgreen';
+    
+    getTable($rows, $cols, $color);
+    ?>
     <!-- Таблица -->
     <!-- Область основного контента -->
   </section>
@@ -100,4 +75,5 @@
     <!-- Нижняя часть страницы -->
   </footer>
 </body>
+
 </html>
