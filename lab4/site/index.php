730015dde5ea971a7123ed15caa6b98f8d91f241
kseniy-san <ksyusankoa@yandex.ru>
2025-11-24 15:42:59 +0300

    23

--- lab4/site/index.php
+++ lab4/site/index.php
@@ -113,7 +113,7 @@ switch ($id) {
 				include 'table.php';
 				break;
 			case 'calc':
-				include 'calc.php';
+				include 'calculator.php';
 				break;
 			default:
 				include 'inc/index.inc.php';
--- lab4/site/table.php
+++ lab4/site/table.php
@@ -8,38 +8,34 @@ $cols = ($cols) ? $cols : 10;
 $rows = ($rows) ? $rows : 10;
 $color = ($color) ? $color : '#ffff00';
 ?>
-
-    <!-- Область основного контента -->
-    <form action=''>
-      <label>Количество колонок: </label>
-      <br>
-      <input name='cols' type='text' value='<?php echo isset($_GET['cols']) ? $_GET['cols'] : ''; ?>'>
-      <br>
-      <label>Количество строк: </label>
-      <br>
-      <input name='rows' type='text' value='<?php echo isset($_GET['rows']) ? $_GET['rows'] : ''; ?>'>
-      <br>
-      <label>Цвет: </label>
-      <br>
-      <input name='color' type='color' value='<?php echo isset($_GET['color']) ? $_GET['color'] : '#ff0000'; ?>' list="listColors">
-      <datalist id="listColors">
+<!-- Область основного контента -->
+<h3>Таблица умножения</h3>
+<form action='<?=$_SERVER['REQUEST_URI']?>' method='POST'>
+    <label>Количество колонок: </label>
+    <br>
+    <input name='cols' type='text' value='<?=isset($_POST['cols']) ? $_POST['cols'] : ''?>'>
+    <br>
+    <label>Количество строк: </label>
+    <br>
+    <input name='rows' type='text' value='<?=isset($_POST['rows']) ? $_POST['rows'] : ''?>'>
+    <br>
+    <label>Цвет: </label>
+    <br>
+    <input name='color' type='color' value='<?=isset($_POST['color']) ? $_POST['color'] : '#ff0000'?>' list="listColors">
+    <datalist id="listColors">
         <option>#ff0000</option>
         <option>#00ff00</option>
         <option>#0000ff</option>
-      </datalist>
-      <br>
-      <br>
-      <input type='submit' value='Создать'>
-    </form>
+    </datalist>
     <br>
-    <!-- Таблица -->
-   <?php 
-    $cols = isset($_GET['cols']) ? (int)$_GET['cols'] : 6;
-    $rows = isset($_GET['rows']) ? (int)$_GET['rows'] : 6;
-    $color = isset($_GET['color']) ? $_GET['color'] : 'lightgreen';
-    
-    getTable($rows, $cols, $color);
-    ?>
-    <!-- Таблица -->
-    <!-- Область основного контента -->
-     
+    <br>
+    <input type='submit' value='Создать'>
+</form>
+<br>
+<!-- Таблица -->
+<?php 
+// Вызываем функцию getTable() с параметрами
+getTable($rows, $cols, $color);
+?>
+<!-- Таблица -->
+<!-- Область основного контента -->
